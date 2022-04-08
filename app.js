f0f33e4676cd6a6e529c9aec1f9db65f4ec89a58
Udith Shyamsukha <udithshyamsukha@gmail.com>
2022-04-08 22:04:57 +0530

    Added seed images

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const getBlogs = async () => {
             let card = document.createElement('div');
             card.setAttribute('class', 'card');
             card.setAttribute('style', 'width: 18rem');
-            card.innerHTML = `<img src="images/book.jpg" class="card-img-top">
+            card.innerHTML = `<img src="Images/book.jpg" class="card-img-top">
         <div class="card-body">
             <h5 class="card-title">${i.title}</h5>
             <a href="#" class="btn btn-primary">Read</a>
