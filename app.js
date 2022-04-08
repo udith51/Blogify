3d6f29bc82ee3ce7c490f730546afcd0176cc323
Udith Shyamsukha <udithshyamsukha@gmail.com>
2022-04-08 21:37:18 +0530

    Added card for each blog

--- app.js
+++ app.js
@@ -7,10 +7,17 @@ const getBlogs = async () => {
         list
         // console.log(res.data);
         for (let i of res.data) {
-            let li = document.createElement('li');
-            li.innerText = i.title;
-            list.appendChild(li);
+            let card = document.createElement('div');
+            card.setAttribute('class', 'card');
+            card.setAttribute('style', 'width: 18rem');
+            card.innerHTML = `<img src="images/book.jpg" class="card-img-top">
+        <div class="card-body">
+            <h5 class="card-title">${i.title}</h5>
+            <a href="#" class="btn btn-primary">Read</a>
+        </div>`
+            list.appendChild(card);
         }
+        console.log(list);
     }
     catch (e) {
         console.log(e);
