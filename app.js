0d720c1fdac9cf5ab0d7d3a2a9d84f2d03337054
Udith Shyamsukha <udithshyamsukha@gmail.com>
2022-04-09 01:19:58 +0530

    Api Call for individual blog

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 console.log('Start');
 const list = document.querySelector('#list');
-console.log(list.innerHTML);
+const content = document.querySelector('.content');
+const details = document.querySelector('.details');
+const title = document.querySelector('#title');
+const b_body = document.querySelector('#b_body');
+const back = document.querySelector('.back');
+const read_area = document.querySelector('#read-area');
+
 const getBlogs = async () => {
     try {
         const res = await axios.get("https://adg-rec-task.herokuapp.com/");
@@ -11,7 +17,7 @@ const getBlogs = async () => {
             card.innerHTML = `<img src="Images/book.jpg" class="card-img-top">
         <div class="card-body">
             <h5 class="card-title">${i.title}</h5>
-            <a href="#" class="btn btn-primary">Read</a>
+            <a href="#" class="read btn btn-primary" id=${i._id} >Read</a>
         </div>`
             list.appendChild(card);
         }
@@ -22,3 +28,34 @@ const getBlogs = async () => {
     }
 }
 getBlogs();
+
+const getABlog = async (id) => {
+    try {
+        const res = await axios.get(`https://adg-rec-task.herokuapp.com/getBlog/${id}`);
+        title.textContent = res.data.title;
+        b_body.innerText = res.data.details;
+        console.log(res.data.title);
+        console.log(res.data.details);
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+
+content.addEventListener('click', (e) => {
+
+    if (e.target.classList.contains('read')) {
+        content.style.display = 'none';
+        details.style.display = 'block';
+        back.style.display = 'block';
+        read_area.style.display = 'block';
+        console.dir(e.target);
+        getABlog(e.target.id);
+    }
+})
+back.addEventListener('click', () => {
+    read_area.style.display = 'none';
+    content.style.display = 'block';
+    details.style.display = 'none';
+    back.style.display = 'none';
+})
